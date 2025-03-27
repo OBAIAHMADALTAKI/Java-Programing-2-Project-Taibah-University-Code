e120e49fbbc5265e178d65ce6e470b4cc2af928c
1sarr <1sarr@DESKTOP-QH4UJP1>
2025-03-27 23:43:40 +0300

    test 5

--- src/StringUpdatePackage/StringUpdate.java
+++ src/StringUpdatePackage/StringUpdate.java
@@ -6,6 +6,9 @@ public class StringUpdate {
     public static void main(String[] args) {
     System.out.println("Hello");
         System.out.println("Obai");
+       
+    }
+    void test() {
+    	System.out.println("Obai");
     }
 }
-//Hello world
