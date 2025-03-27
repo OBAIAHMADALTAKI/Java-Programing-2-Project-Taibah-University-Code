0511ee8c0a88d7d36c27ca50a23814b941038ce7
1sarr <1sarr@DESKTOP-QH4UJP1>
2025-03-27 23:46:40 +0300

    The beginning of the project

--- src/StringUpdatePackage/StringUpdate.java
+++ src/StringUpdatePackage/StringUpdate.java
@@ -1,14 +1,5 @@
-
 package StringUpdatePackage;
 
-
 public class StringUpdate {
-    public static void main(String[] args) {
-    System.out.println("Hello");
-        System.out.println("Obai");
-       
-    }
-    void test() {
-    	System.out.println("Obai");
-    }
+    
 }
