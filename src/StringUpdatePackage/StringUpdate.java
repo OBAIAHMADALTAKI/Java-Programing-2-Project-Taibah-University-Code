e862c38670e1cfcfc6c4a40dfccc50fd392ad45d
OBAIAHMADALTAKI <obaiahmad.university@gmail.com>
2025-03-27 23:26:23 +0300

    test 2

--- src/StringUpdatePackage/StringUpdate.java
+++ src/StringUpdatePackage/StringUpdate.java
@@ -5,5 +5,6 @@ package StringUpdatePackage;
 public class StringUpdate {
     public static void main(String[] args) {
     System.out.println("Hello");
+        System.out.println("Obai");
     }
 }
